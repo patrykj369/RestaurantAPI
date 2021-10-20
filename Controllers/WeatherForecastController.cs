2a00b04a62f5541eda950e9f231749a46efa4765
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-10-20 22:15:30 +0200

    cleaned code

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -37,21 +37,6 @@ namespace RestaurantAPI.Controllers
             
             
         }
-
-        /*[HttpGet("currentDay/{max}")]
-        public IEnumerable<WeatherForecast> Get2([FromQuery]int take, [FromRoute]int max)
-        {
-            var result = _service.Get();
-            return result;
-        }*/
-
-        /*[HttpPost]
-        public ActionResult<string> Hello([FromBody]string name)
-        {
-            //HttpContext.Response.StatusCode = 401;
-            //return $"Hello {name}";
-
-            return NotFound($"Hello {name}");
-        }*/
+        
     }
 }
