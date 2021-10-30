c05953611d5268768e0078cbaf655986551f0099
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-10-30 22:51:54 +0200

    added new user account to DB

---
+++ Services/AccountService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.Services
+{
+    public interface IAccountService
+    {
+        public void RegisterUser(RegisterUserDto dto);
+    }
+
+    public class AccountService : IAccountService
+    {
+        private readonly RestaurantDbContext _context;
+
+        public AccountService(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        public void RegisterUser(RegisterUserDto dto)
+        {
+            var newUser = new User()
+            {
+                Email = dto.Email,
+                DateOfBirth = dto.DateOfBirth,
+                Nationality = dto.Nationality,
+                RoleId = dto.RoleId,
+
+            };
+
+            _context.AddRange(newUser);
+            _context.SaveChanges();
+        }
+    }
+}
