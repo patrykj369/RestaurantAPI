ff43c74d59e5534749338e7fe3215dddc96380ab
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-10-31 23:20:57 +0100

    added jwt tokens

--- Middleware/ErrorHandlingMiddleware.cs
+++ Middleware/ErrorHandlingMiddleware.cs
@@ -23,6 +23,11 @@ namespace RestaurantAPI.Middleware
             {
                 await next.Invoke(context);
             }
+            catch (BadRequestException badRequestException)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync(badRequestException.Message);
+            }
             catch (NotFoundExceptions notFoundException)
             {
                 context.Response.StatusCode = 404;
