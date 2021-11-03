f2bc7b7d416f487e703f3bc8a406243ab5eac141
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-11-03 21:45:59 +0100

    started work on minimum restaurant added restriction for authorization

---
+++ Authorization/MinimumAddedRestaurantCount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace RestaurantAPI.Authorization
+{
+    public class MinimumAddedRestaurantCount : IAuthorizationRequirement
+    {
+        public int MinimumCountRestaurantAdd { get; }
+
+        public MinimumAddedRestaurantCount(int minimumCountRestaurantAdd)
+        {
+            MinimumCountRestaurantAdd = minimumCountRestaurantAdd;
+        }
+    }
+}
---
+++ Authorization/MinimumAddedRestaurantCountHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using RestaurantAPI.Entities;
+
+namespace RestaurantAPI.Authorization
+{
+    public class MinimumAddedRestaurantCountHandler : AuthorizationHandler<MinimumAddedRestaurantCount>
+    {
+        private readonly RestaurantDbContext _dbContext;
+
+        public MinimumAddedRestaurantCountHandler(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MinimumAddedRestaurantCount requirement)
+        {
+            var user = context.User.
+            var addedRestaurant = _dbContext.Restaurants.Where(x => x.CreatedById == context.User).GroupBy(z => z);
+        }
+    }
+}
